3606a6e5bbc037dc7b7ef7e563014e04dbe1b64e
AndrewWayne <AndrewWayne2016@gmail.com>
2019-05-17 22:57:37 +0800

    1

---
+++ Main.cpp
@@ -0,0 +1,51 @@
+/*
+ * Author: xiaohei_AWM
+ * Date:
+ * Mutto: Face to the weakness, expect for the strength.
+*/
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<utility>
+#include<functional>
+#include<cmath>
+#include<vector>
+#include<assert.h>
+using namespace std;
+#define reg register
+#define endfile fclose(stdin);fclose(stdout);
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef std::pair<int,int> pii;
+typedef std::pair<ll,ll> pll;
+namespace IO{
+    char buf[1<<15],*S,*T;
+    inline char gc(){
+        if (S==T){
+            T=(S=buf)+fread(buf,1,1<<15,stdin);
+            if (S==T)return EOF;
+        }
+        return *S++;
+    }
+    inline int read(){
+        reg int x;reg bool f;reg char c;
+        for(f=0;(c=gc())<'0'||c>'9';f=c=='-');
+        for(x=c^'0';(c=gc())>='0'&&c<='9';x=(x<<3)+(x<<1)+(c^'0'));
+        return f?-x:x;
+    }
+    inline ll readll(){
+        reg ll x;reg bool f;reg char c;
+        for(f=0;(c=gc())<'0'||c>'9';f=c=='-');
+        for(x=c^'0';(c=gc())>='0'&&c<='9';x=(x<<3)+(x<<1)+(c^'0'));
+        return f?-x:x;
+    }
+}
+using namespace IO;
+int main(){
+
+    return 0;
+}
